6c9a60ec5097e7860bca45e79ba121eaa1e42295
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 15:50:36 +0530

    add target and rel to links

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <a href="https://twitter.com/Mathe4Sam" className="social-links">
+      <a href="https://twitter.com/Mathe4Sam" className="social-links" target="_blank" rel="noopener noreferrer">
         <i class="fa-brands fa-square-x-twitter "></i>
       </a>
-      <a href="https://www.instagram.com/_sam_g.m/" className="social-links">
+      <a href="https://www.instagram.com/_sam_g.m/" className="social-links" target="_blank" rel="noopener noreferrer">
         <i class="fa-brands fa-square-instagram"></i>
       </a>
-      <a href="https://github.com/SGM-dev" className="social-links">
+      <a href="https://github.com/SGM-dev" className="social-links" target="_blank" rel="noopener noreferrer">
         <i class="fa-brands fa-square-github"></i>
       </a>
     </footer>
