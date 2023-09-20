69e0ef0b6ac97328dab396e40c7af3d290c86959
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 15:32:45 +0530

    update content

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <section className="section">
           <h3 className="section-title">About</h3>
-          <p className="section-content">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>  
+          <p className="section-content">I am a Software Engineer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>  
         </section>
         )
 }
--- src/Interests.jsx
+++ src/Interests.jsx
@@ -4,10 +4,7 @@ export default function Interests() {
   return (
     <section className="section">
       <h3 className="section-title">Interests</h3>
-      <p className="section-content">
-        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
-        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
-      </p>
+      <p className="section-content">Reader. Internet fanatic. Foodie.</p>
     </section>
   );
 }
