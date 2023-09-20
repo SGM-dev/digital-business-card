6472f1b9e5111ae57ee82cfccf1e357afc75cd19
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 14:11:53 +0530

    initialize index with strict mode

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
