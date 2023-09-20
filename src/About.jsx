723dfbead82439172c77ada9bc036f75dd748617
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 15:10:57 +0530

    Build About component

--- src/About.jsx
+++ src/About.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function About() {
-    return <h1>About Component</h1>
+    return (
+        <section className="section">
+          <h3 className="section-title">About</h3>
+          <p className="section-content">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>  
+        </section>
+        )
 }
