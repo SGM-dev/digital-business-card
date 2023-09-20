90b252cb08513dac99fca89ea10b43bdb36c4db3
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 21:18:26 +0530

    add styling for screens larger than 768px

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
     return (
-        <section className="section">
+        <section className="section about">
           <h3 className="section-title">About</h3>
           <p className="section-content">I am a Software Engineer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>  
         </section>
--- src/Interests.jsx
+++ src/Interests.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Interests() {
   return (
-    <section className="section">
+    <section className="section interests">
       <h3 className="section-title">Interests</h3>
       <p className="section-content">Reader. Internet fanatic. Foodie.</p>
     </section>
