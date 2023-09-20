0d4628acffd854ab904f540bdad86b27ee650324
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 14:59:26 +0530

    build Information component

--- src/Information.jsx
+++ src/Information.jsx
@@ -5,8 +5,20 @@ import profile from "./assets/profile.jpg";
 export default function Information() {
   return (
     <header>
-      <img src={profile} alt="Photo of Sam"  className="info--img"/>
-  
+      <img src={profile} alt="Photo of Sam" className="info-img" />
+      <h1 className="info-name">Sam George Mathew</h1>
+      <h2 className="info-role">Software Engineer</h2>
+      <a href="sgmfs.dev" className="info-website">
+        sgmfs.dev
+      </a>
+      <div className="contact-btns">
+        <button className="btn mail-btn">
+          <i class="fa-solid fa-envelope"></i>Email
+        </button>
+        <button className="btn linkedin-btn">
+          <i class="fa-brands fa-linkedin"></i>LinkedIn
+        </button>
+      </div>
     </header>
   );
 }
