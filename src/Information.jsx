20c01333259a68cab1149b363fd674e70fd05e9d
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 15:44:08 +0530

    update Information

--- src/Information.jsx
+++ src/Information.jsx
@@ -12,10 +12,13 @@ export default function Information() {
         sgmfs.dev
       </a>
       <div className="contact-btns">
-        <button className="btn mail-btn">
+        <button className="btn mail-btn" href="1a3jwr08d@mozmail.com">
           <i class="fa-solid fa-envelope"></i>Email
         </button>
-        <button className="btn linkedin-btn">
+        <button
+          className="btn linkedin-btn"
+          href="https://www.linkedin.com/in/sam-george-mathew-b17b39118/"
+        >
           <i class="fa-brands fa-linkedin"></i>LinkedIn
         </button>
       </div>
