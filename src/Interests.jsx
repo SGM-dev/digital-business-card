f23414f81f004b75782e38656153d3dda2d784c7
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 15:13:16 +0530

    Build Interest Component

--- src/Interests.jsx
+++ src/Interests.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Interests() {
-    return <h1>Interests Component</h1>
+  return (
+    <section className="section">
+      <h3 className="section-title">Interests</h3>
+      <p className="section-content">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </section>
+  );
 }
