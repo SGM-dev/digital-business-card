39cd284ddca27763defcbb45aa4930acf7f98e86
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-20 15:50:57 +0530

    update links

--- src/Information.jsx
+++ src/Information.jsx
@@ -8,19 +8,31 @@ export default function Information() {
       <img src={profile} alt="Photo of Sam" className="info-img" />
       <h1 className="info-name">Sam George Mathew</h1>
       <h2 className="info-role">Software Engineer</h2>
-      <a href="https://www.sgmfs.dev" className="info-website">
+      <a
+        href="https://www.sgmfs.dev"
+        className="info-website"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         sgmfs.dev
       </a>
       <div className="contact-btns">
-        <button className="btn mail-btn" href="1a3jwr08d@mozmail.com">
+        <a
+          className="btn mail-btn"
+          href="mailto:1a3jwr08d@mozmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i class="fa-solid fa-envelope"></i>Email
-        </button>
-        <button
+        </a>
+        <a
           className="btn linkedin-btn"
           href="https://www.linkedin.com/in/sam-george-mathew-b17b39118/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <i class="fa-brands fa-linkedin"></i>LinkedIn
-        </button>
+        </a>
       </div>
     </header>
   );
